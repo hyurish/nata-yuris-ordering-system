4c1509350f20d0b28f0f9de70df74e2c8cf1fe61
yuris <yuris.borispol@gmail.com>
2014-07-31 02:22:25 +0300

    MenuServiceTests added

---
+++ restaurant/src/test/java/ua/yuris/restaurant/service/MenuServiceTests.java
@@ -0,0 +1,11 @@
+package ua.yuris.restaurant.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yuris
+ * Date: 31.07.14
+ * Time: 2:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class MenuServiceTests {
+}
